d00deb4ff46c6e54acc2597da64b1b219a3cbec8
sam <3161728@gmail.com>
2020-12-14 15:57:54 +0900

    Add TypeORM test entitiy

--- src/restaurants/entities/restaurant.entitiy.ts
+++ src/restaurants/entities/restaurant.entitiy.ts
@@ -1,16 +1,30 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@ObjectType()
+@ObjectType() // for build graphql schema
+@Entity() // for TypeORM
 export class Restaurant {
+  @PrimaryGeneratedColumn()
+  @Field(() => Number)
+  id: number;
+
   @Field(() => String)
+  @Column()
   name: string;
 
   @Field(() => Boolean)
+  @Column()
   isVegan: boolean;
 
   @Field(() => String)
+  @Column()
   address: string;
 
   @Field(() => String)
+  @Column()
   ownerName: string;
+
+  @Field(() => String)
+  @Column()
+  categoryName: string;
 }
