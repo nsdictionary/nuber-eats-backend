4360783d275de05aa43051aa3395c0ebeecb32aa
sam <3161728@gmail.com>
2020-12-15 23:40:32 +0900

    Add middleware and test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,9 @@
-import { Module } from "@nestjs/common";
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from "@nestjs/common";
 import * as Joi from "joi";
 import { ConfigModule } from "@nestjs/config";
 import { GraphQLModule } from "@nestjs/graphql";
