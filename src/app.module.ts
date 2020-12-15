4d47ec1893b33fd4e0e02e8816650d94ad307a2a
sam <3161728@gmail.com>
2020-12-15 21:12:37 +0900

    Add options to jwt custom module

--- src/app.module.ts
+++ src/app.module.ts
@@ -40,7 +40,9 @@ import { JwtModule } from "./jwt/jwt.module";
     }),
     UsersModule,
     CommonModule,
-    JwtModule.forRoot(),
+    JwtModule.forRoot({
+      privateKey: process.env.SECRET_KEY,
+    }),
   ],
   controllers: [],
   providers: [],
