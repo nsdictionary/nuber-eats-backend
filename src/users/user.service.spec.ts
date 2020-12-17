5cabf8f9555ed3c8d79f0ff598ae0ea2c4b9624a
sam <3161728@gmail.com>
2020-12-17 11:50:25 +0900

    Add create new user test case

--- src/users/user.service.spec.ts
+++ src/users/user.service.spec.ts
@@ -7,12 +7,12 @@ import { JwtService } from "../jwt/jwt.service";
 import { MailService } from "../mail/mail.service";
 import { Repository } from "typeorm";
 
-const mockRepository = {
+const mockRepository = () => ({
   findOne: jest.fn(),
   save: jest.fn(),
   create: jest.fn(),
   delete: jest.fn(),
-};
+});
 
 const mockJwtService = {
   sign: jest.fn(),
@@ -35,11 +35,11 @@ describe("UserService", () => {
         UsersService,
         {
           provide: getRepositoryToken(User),
-          useValue: mockRepository,
+          useValue: mockRepository(),
         },
         {
           provide: getRepositoryToken(Verification),
-          useValue: mockRepository,
+          useValue: mockRepository(),
         },
         {
           provide: JwtService,
@@ -60,22 +60,34 @@ describe("UserService", () => {
   });
 
   describe("createAccount", () => {
+    const createAccountArgs = {
+      email: "",
+      password: "",
+      role: 0,
+    };
+
     it("should fail if user exist", async () => {
       usersRepository.findOne.mockResolvedValue({
         id: 1,
         email: "test@test.com",
       });
 
-      const result = await service.createAccount({
-        email: "",
-        password: "",
-        role: 0,
-      });
+      const result = await service.createAccount(createAccountArgs);
 
       expect(result).toMatchObject({
         ok: false,
         error: "There is a user with that email already",
       });
+    });
+
+    it("should create a new user", async () => {
+      usersRepository.findOne.mockResolvedValue(undefined);
+      usersRepository.create.mockReturnValue(createAccountArgs);
+      await service.createAccount(createAccountArgs);
+      expect(usersRepository.create).toHaveBeenCalledTimes(1);
+      expect(usersRepository.create).toHaveBeenCalledWith(createAccountArgs);
+      expect(usersRepository.save).toHaveBeenCalledTimes(1);
+      expect(usersRepository.save).toHaveBeenCalledWith(createAccountArgs);
     });
   });
 
