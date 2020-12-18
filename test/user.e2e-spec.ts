96473f75852ade2a178a7b98b75d498b2f836b3b
sam <3161728@gmail.com>
2020-12-18 11:44:10 +0900

    Testing create account

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -4,6 +4,14 @@ import * as request from "supertest";
 import { AppModule } from "./../src/app.module";
 import { getConnection } from "typeorm";
 
+jest.mock("got", () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
+const GRAPHQL_ENDPOINT = "/graphql";
+
 describe("UserModule (e2e)", () => {
   let app: INestApplication;
 
@@ -21,7 +29,58 @@ describe("UserModule (e2e)", () => {
     await app.close();
   });
 
-  it.todo("createAccount");
+  describe("createAccount", () => {
+    const EMAIL = "test@test.com";
+
+    it("should create account", () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(data:{
+              email: "${EMAIL}",
+              password: "1234",
+              role: Owner,
+            }) {
+              ok
+              error
+            }
+          }`,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    it("should fail if account already exists", () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(data: {
+              email:"${EMAIL}",
+              password:"1234",
+              role:Owner
+            }) {
+              ok
+              error
+            }
+          } `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toBe(
+            "There is a user with that email already"
+          );
+        });
+    });
+  });
+
   it.todo("userProfile");
   it.todo("login");
   it.todo("me");
